7fb82bad2eb72ebca2aad56ffda2fb492425e98a
Nithish Vengatesan <nithishvengatesan41@gmail.com>
2025-12-16 06:35:00 +0000

    Basic Template Push

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.kaar.qualityportal.controller.View1", {
+        onInit() {
+        }
+    });
+});
